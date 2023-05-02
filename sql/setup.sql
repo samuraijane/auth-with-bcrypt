769f3886827af312217a9c9fa70222315eb52255
Dezarea Bryan <dezareabryan@gmail.com>
2023-05-02 16:56:26 -0400

    bcrypt setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE demo_bcrypt;
 \c demo_bcrypt
 CREATE TABLE students (
-    id serial primary key,
-    firstName varchar(50) not null,
-    lastName varchar(50) not null,
-    email varchar(50) not null,
-    password varchar(50) not null,
-    createdAt timestamp not null
-    updatedAt timestamp not null
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 INSERT INTO students (
-  "id",
-  "firstName",
-  "lastName",
-  "email",
-  "password",
-  "createdAt",
-  "updatedAt"
+  id,
+  first_name,
+  last_name,
+  email,
+  password,
+  created_at,
+  updated_at
 ) VALUES (
   1,
   'Anna',
   'Anderson',
   'anna@email.com',
   'pass1234',
-  CURRENT_TIMESTAMP(3),
-  CURRENT_TIMESTAMP(3)
+  NOW(),
+  NOW()
 );
